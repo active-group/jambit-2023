81cbe4bbcfb74c7a6acabd0924e31f85999f133a
Kaan Sahin <kaan.sahin@active-group.de>
2023-03-24 14:03:09 +0100

    Update.

--- haskell-code/DB.hs
+++ haskell-code/DB.hs
@@ -1,1 +1,55 @@
 module DB where
+
+{- 
+DB: Key-Value-Store: String -> Integer
+
+put "Kaan" 35
+x = get "Kaan"
+put "Kaan" (x+1)
+y = get "Kaan"
+return (show (x+y))
+
+-}
+{-
+data DBCommand a =
+      Put String Integer
+    | Get String
+    | Return a
+
+type DBProgram a = [DBCommand a]
+
+p1 = [Put "Kaan" 35, Get "Kaan", Put "Kaan" (x+1)]
+-- leider doch nicht möglich, weil wir 
+-- - keinen namen vergeben können
+-- - einzelne kommandos nicht verknüpft
+
+-}
+
+-- Callback!
+
+data DB a =
+      Put String Integer (()      -> DB a)
+    | Get String         (Integer -> DB a)
+    | Return a
+
+p1 = Put "Kaan" 35    (\() ->
+     Get "Kaan"       (\x ->
+     Put "Kaan" (x+1) (\() -> 
+     Get "Kaan"       (\y ->
+     Return (show (x+y)))))) 
+
+get key = Get key Return
+
+put key val = Put key val Return
+
+smallProgram = get "Kaan"
+
+smallProgram2 = put "Kaan" 35
+
+splice :: DB a -> (a -> DB b) -> DB b
+splice (Return a) next             = next a
+splice (Get key callback) next     = 
+    Get key (\value -> splice (callback value) next)
+splice (Put key val callback) next =
+    Put key val (\() -> splice (callback ()) next)
+
